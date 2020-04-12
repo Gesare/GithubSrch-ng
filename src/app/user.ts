1fa8ef8e862652b9398e41940c36a68ab9afd9a4
Gesare <gesareayienda@gmail.com>
2020-04-12 20:34:33 +0300

    Added text

--- src/app/user.ts
+++ src/app/user.ts
@@ -1,6 +1,6 @@
 export class User {
     // public showListRepo:boolean;
-    constructor(public login: string,public name:string, public html_url:string,public avatar_url:string, public public_repos:number){
+    constructor(public login: string,public name:string, public html_url:string,public avatar_url:string, public public_repos:number,public key:string){
         // this.showListRepo=false;
     }
 }
