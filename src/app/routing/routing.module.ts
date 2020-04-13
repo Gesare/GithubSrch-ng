359191212ee014d277902952babd63dab175820a
Gesare <gesareayienda@gmail.com>
2020-04-13 12:21:46 +0300

    Changes to the file

--- src/app/routing/routing.module.ts
+++ src/app/routing/routing.module.ts
@@ -1,18 +1,1 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { Routes, RouterModule } from '@angular/router';
-import { UserComponent } from './user/user.component';
-import { UserformComponent } from './userform/userform.component';
 
-const routes: Routes = [
-{path:'userform', component: UserformComponent},
-{path:'user/:username', component: UserComponent}
-];
-
-@NgModule({
-  imports: [CommonModule,
-    RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-
-export class RoutingModule { }
