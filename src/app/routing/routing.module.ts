c940a060a210cafa3658d2aab5121cc24892998e
Gesare <gesareayienda@gmail.com>
2020-04-13 13:13:41 +0300

    Fixed some errors

--- src/app/routing/routing.module.ts
+++ src/app/routing/routing.module.ts
@@ -1,2 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {RouterModule,Routes} from '@angular/router';
 
+import { UserformComponent } from '../userform/userform.component';
+import { UserComponent } from '../user/user.component';
 
+const routes: Routes = [
+    {path:'userform', component: UserformComponent},
+    {path:'user/:username', component: UserComponent}
+    ];
+    
+    @NgModule({
+      imports: [CommonModule,
+        RouterModule.forRoot(routes)],
+      exports: [RouterModule],
+      declarations: []
+    })
+
+export class RoutingModule { }
